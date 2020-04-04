9570b08ea0647bac4c24083dff5fb57541c88cd9
Ryan Birmingham <birm@rbirm.us>
2020-04-04 00:50:49 -0400

    footprint

---
+++ src/Smoothing.js
@@ -0,0 +1,22 @@
+// single double and triple exponential smoothing
+class Smoothing{
+  constructor(data, params){
+    this.data = data
+    this.params = params
+  }
+  fit(){
+
+  }
+  predict(){
+
+  }
+}
+
+class SingleSmooth extends Smoothing{
+}
+
+class DoubleSmooth extends Smoothing{
+}
+
+class TripleSmooth extends Smoothing{
+}
