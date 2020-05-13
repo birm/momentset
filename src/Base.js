31350615b01755c47f710e426f2d877908c97d15
Ryan Birmingham <birm@rbirm.us>
2020-05-12 23:09:49 -0400

    autoCor and deps

--- src/Base.js
+++ src/Base.js
@@ -17,15 +17,64 @@ function devSeed(observations) {
 
 // stats related functions
 function mean(X){
-  return 0
+  let res = 0.0
+  for (let i in X){
+    res += parseFloat(i)
+  }
+  return res/X.length
+}
+function covariance(X, Y){
+  if (X.length != Y.length) {
+      console.info("covariance length mismatch") // expected for autoCorrelation
+  }
+  let N = Math.min(X.length, Y.length)
+  let res = 0
+  let xu = mean(X)
+  let yu = mean(Y)
+  for (let i=0; i<N; i++){
+    res+= (X[i]-xu)*(Y[i]-yu)
+  }
+  return res/N
 }
 
-function correlation(X){
-  return 0
+const cov = covariance // alias
+
+function variance(X){
+  let res = 0
+  let xu = mean(X)
+  for (let i in X){
+    res += Math.pow((parseFloat(i) - xu), 2)
+  }
+  return res/X.length
 }
 
-function autoCorrelation(X, k){
-  return 0
+// const var = variance // alias we can't use for obvious reasons. Sorry!
+
+function standardDeviation(X, silent){
+  let res = variance(X)
+  if (!silent && res < 0){
+    throw new Error("No complex Support")
+  }
+  return Math.pow(res, 0.5)
+}
+
+function lag(X, t){
+  if (X.length < t){
+    console.warn("Not enough observations to lag: " + X.length + " < " + t)
+  }
+  return X.slice(t)
+}
+
+const std = standardDeviation // alias
+
+function correlation(X, Y){
+  return covariance(X, Y)/(standardDeviation(X) * standardDeviation(Y))
+}
+
+const corr = correlation // alias
+
+function autoCorrelation(X, t){
+  return correlation(X, lag(X,t))
 }
 
 
