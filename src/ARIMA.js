f8490d60dad5928a59843b73869dd88a0ec658e3
Ryan Birmingham <birm@rbirm.us>
2020-05-20 23:22:44 -0400

    implment MA

--- src/ARIMA.js
+++ src/ARIMA.js
@@ -2,8 +2,6 @@ class AutoRegression extends Model {
     constructor(data, params) {
         super(data, params)
         this.p = this.params.p || 1
-        this.X = data
-        this.params = params
     }
     fit() {
         // using yule walker
@@ -11,7 +9,7 @@ class AutoRegression extends Model {
         let R = [...Array(this.p)].map(e => Array(this.p));
         // populate R
         for (let j = 0; j< this.p; j++){
-          r[j] = acf(this.X, j)
+          r[j] = acf(this.data, j)
           for (let k=0; k< this.p; k++){
             let kk = (k + j) % this.p
             R[k][kk] = r[j]
@@ -21,22 +19,21 @@ class AutoRegression extends Model {
         // todo, consider removing math.js dep by implementing inv and multiply
         let Ri = math.inv(math.matrix(R))
         this.Phi = math.multiply(Ri, r)._data
-        return this.X // does NOT smooth
+        return this.data // does NOT smooth
     }
     forecast(n) {
-      let T = this.X.slice(this.X.length - this.Phi.length)
+      let T = this.data.slice(this.data.length - this.Phi.length)
         for (let i=0; i < n; i++){
           let x = 0
           for (let j=0; j < this.Phi.length; j++){
             x += this.Phi[j] * T[T.length - j - 1]
           }
           T.push(x)
-          //console.log(T)
         }
         return T.slice(this.Phi.length) // only return predictions
     }
     append(pt) {
-        this.X.push(pt)
+        this.data.push(pt)
         this.fit() // recalculate fit
         return pt // no change, does not smooth
     }
@@ -45,16 +42,26 @@ class AutoRegression extends Model {
 class MovingAverage extends Model {
     constructor(data, params) {
         super(data, params)
-        this.alpha = this.params.alpha || 0.8
+        this.q = this.params.q || 5
     }
     fit() {
-        throw new Error("Not Implemented")
+        this.X = []
+        for (let i=0; i<this.data.length; i++){
+          let ma = this.data.slice(Math.max((i-this.q) + 1, 0), i + 1)
+          this.X.push(ma.reduce((a,b)=>a+b) / ma.length)
+        }
+        return this.X
     }
     forecast(n) {
-        throw new Error("Not Implemented")
+        return new Array(n).fill(this.X[this.X.length - 1])
     }
     append(pt) {
-        throw new Error("Not Implemented")
+        let i = this.data.length
+        let ma = this.data.slice(Math.max((i-this.q) + 1, 0), i )
+        ma.push(pt)
+        let res = ma.reduce((a,b)=>a+b) / ma.length
+        this.data.push(res)
+        return res
     }
 }
 
